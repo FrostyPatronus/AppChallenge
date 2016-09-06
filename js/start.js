e668d3c0d5358a48823f320158435dcc4c732938
Timothy Samson <t.samson@nvusd.org>
2016-09-06 11:13:09 -0700

    Overlayed the start div to the main page

--- js/start.js
+++ js/start.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var $start_div = $("#start"); 
 
+    // If the user clicks on anywhere on the start page,
+    // id=start will disappear and be removed
     $start_div.click(function() {
         $start_div.addClass("animated fadeOut");
         $start_div.one(CONST.get("ON_ANIMATION_END"), function(){
