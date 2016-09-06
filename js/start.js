a8f7cfb6596e7fae49284dc6bba486c7ef3dd270
Timothy Samson <t.samson@nvusd.org>
2016-09-06 13:48:16 -0700

    Added the main page. Started making DRY includes

--- js/start.js
+++ js/start.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     var $start_div = $("#start"); 
 
     // If the user clicks on anywhere on the start page,
-    // id=start will disappear and be removed
+    // id=start will disappear and be removed.
+    // It will then reload to 
     $start_div.click(function() {
         $start_div.addClass("animated fadeOut");
         $start_div.one(CONST.get("ON_ANIMATION_END"), function(){
             $start_div.remove();
+            window.location.replace("main.html");
         });
-
-
     });
 });
