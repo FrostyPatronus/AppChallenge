7433afd4ac22df9246170a2866b4dfdb9a41f541
Timothy Samson <t.samson@nvusd.org>
2016-09-05 22:19:32 -0700

    Added Fade-out functionality.

---
+++ js/constants.js
@@ -0,0 +1,29 @@
+// WE DEFINE SOME CONSTANTS RIGHT HERE.
+
+/*
+
+// USAGE: // 
+If you want to use the constant, say, 'ON_ANIMATION_END', you gotta
+include this file in your javascript file OR you can include this 
+in your HTML file BEFORE the JS file you wanna use constants in
+
+After that, to use 'ON_ANIMATION_END', type in
+CONSTANT.get('ON_ANIMATION_END');
+Etc...
+
+// TO ADD CONSTANTS //
+In the  'var private' section, add a comma after the last constants entry and
+type in:
+<CONSTANT NAME>: <CONSTANT VALUE>
+
+*/
+
+var CONST = (function() {
+     var private = {
+        'ON_ANIMATION_END':'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+     };
+
+     return {
+        get: function(name) { return private[name]; }
+    };
+})();
--- js/start.js
+++ js/start.js
@@ -1,3 +1,12 @@
-$(".start").keypress(function(){
-    
+$(document).ready(function() {
+    var $start_div = $("#start"); 
+
+    $start_div.click(function() {
+        $start_div.addClass("animated fadeOut");
+        $start_div.one(CONST.get("ON_ANIMATION_END"), function(){
+            $start_div.remove();
+        });
+
+
+    });
 });
