d10daf8ace2f74421b3d1a961549bad3ef644769
Timothy Samson <t.samson@nvusd.org>
2016-10-22 14:51:27 -0700

    Added classroom controller

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
-$(function(){
-    $("#draggable").draggable({
-        containment: "#canvas",
-        grid: [30, 30]
+(function(){
+    // Makes a div with class "draggable" draggable and resizable
+    // within a 30x30 grid
+    $(".draggable")
+        .draggable({
+            containment: "#canvas",
+            grid: [30, 30]
+        })
+        .resizable({
+            grid: [30, 30]
+        });
+})();
+
+(function() {
+    var app = angular.module('ClassroomModule', []);
+
+    app.controller('ClassroomController', function($scope){
+        
+        $scope.tables = [{name: "Hello"}, {name: "Hi"}];
+
+        $scope.addTable = function(){
+            $scope.tables.push({
+                name: "Hello"
+            });
+        };
+
+        // angular.element(document).ready(function(){
+        //     $(".draggable")
+        //         .draggable({
+        //             containment: "#canvas",
+        //             grid: [30, 30]
+        //         })
+        //         .resizable({
+        //             grid: [30, 30]
+        //         });
+        // });
+
     });
-    $("#draggable").resizable({
-        grid: [30, 30]
-    });
-});
+})();
+
 
