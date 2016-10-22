73e0a1edb1365c61d16021c0513cf02ac9b7c498
Timothy Samson <t.samson@nvusd.org>
2016-10-21 21:02:34 -0700

    Added draggable and resizable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-$("#draggable").draggable({
-    containment: "#canvas",
-    grid: [30, 30]
+$(function(){
+    $("#draggable").draggable({
+        containment: "#canvas",
+        grid: [30, 30]
+    });
+    $("#draggable").resizable({
+        grid: [30, 30]
+    });
 });
+
