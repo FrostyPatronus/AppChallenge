09b0a9e49daf1b0804c2a7b63c911e463783cb10
Timothy Samson <t.samson@nvusd.org>
2016-10-23 15:56:04 -0700

    Added bower and angular gridster

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-function makeDraggable(){
-    // Makes a div with class "draggable" draggable and resizable
-    // within a 30x30 grid
-    $(".draggable")
-        .draggable({
-            containment: "#canvas",
-            grid: [30, 30]
-        })
-        .resizable({
-            containment: "#canvas",
-            grid: [30, 30]
+// function makeDraggable(){
+//     // Makes a div with class "draggable" draggable and resizable
+//     // within a 30x30 grid
+//     $(".draggable")
+//         .draggable({
+//             containment: "#canvas",
+//             grid: [30, 30]
+//         })
+//         .resizable({
+//             containment: "#canvas",
+//             handles: "all",
+//             grid: [30, 30]
 
-        });
-}
+//         });
+// }
 
 (function() {
-    var app = angular.module('ClassroomModule', []);
+    var app = angular.module('ClassroomModule', ['gridster']);
 
     app.controller('ClassroomController', ["$scope", function($scope){
 
@@ -25,6 +26,20 @@ function makeDraggable(){
                 name: "Hello"
             });
         };
+
+        $scope.items = [
+          { sizeX: 2, sizeY: 1, row: 0, col: 0 },
+          { sizeX: 2, sizeY: 2, row: 0, col: 2 },
+          { sizeX: 1, sizeY: 1, row: 0, col: 4 },
+          { sizeX: 1, sizeY: 1, row: 0, col: 5 },
+          { sizeX: 2, sizeY: 1, row: 1, col: 0 },
+          { sizeX: 1, sizeY: 1, row: 1, col: 4 },
+          { sizeX: 1, sizeY: 2, row: 1, col: 5 },
+          { sizeX: 1, sizeY: 1, row: 2, col: 0 },
+          { sizeX: 2, sizeY: 1, row: 2, col: 1 },
+          { sizeX: 1, sizeY: 1, row: 2, col: 3 },
+          { sizeX: 1, sizeY: 1, row: 2, col: 4 }
+        ];
 
     }]);
 
