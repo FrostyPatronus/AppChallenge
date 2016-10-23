3f5e3b1472f4da1857216f864a43e903da1c8f4a
Timothy Samson <t.samson@nvusd.org>
2016-10-22 23:13:06 -0700

    Added restraints for resize

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function(){
+function makeDraggable(){
     // Makes a div with class "draggable" draggable and resizable
     // within a 30x30 grid
     $(".draggable")
@@ -7,16 +7,17 @@
             grid: [30, 30]
         })
         .resizable({
+            containment: "#canvas",
             grid: [30, 30]
         });
-})();
+}
 
 (function() {
     var app = angular.module('ClassroomModule', []);
 
     app.controller('ClassroomController', ["$scope", function($scope){
 
-        $scope.tables = [{name: "Hello"}, {name: "Hi"}];
+        $scope.tables = [];
 
         $scope.addTable = function(){
             $scope.tables.push({
@@ -29,7 +30,10 @@
     app.directive('table', function(){
         return {
             restrict: 'E',
-            templateUrl: 'html/table.html'
+            templateUrl: 'html/table.html', 
+            link: function(scope, element, attr){
+                makeDraggable();
+            }
         };
     });
 })();
