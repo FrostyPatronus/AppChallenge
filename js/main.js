28bd581c9ac829a80479182e1e31320fdc60fdfa
Timothy Samson <t.samson@nvusd.org>
2016-10-28 17:19:15 -0700

    Added change shade feature

--- js/main.js
+++ js/main.js
@@ -1,60 +1,95 @@
+function toggleTableStyle(editMode) {
+    if (editMode) {
+        $(".table-handle").css("cursor", "grabbing");
+        $(".table").addClass("black-border");
+    } else {
+        $(".table").css("cursor", "pointer");
+        $(".table").removeClass("black-border");
+    }
+}
+
+function changeShade(points) {
+    var element = $(".table-points");
+
+    var shade = 0 + (points * 25);
+
+    var rgbString = "rgb(";
+    rgbString += "0, ";
+    rgbString += shade + ", ";
+    rgbString += "0)";
+
+    element.css("color", rgbString);
+}
+
 (function() {
     var app = angular.module('ClassroomModule', ['gridster']);
 
     app.controller('ClassroomController', ["$scope", function($scope){
+        // Determines if the app is in edit mode
         $scope.editMode = false;
 
+        // Watches for changes in the scope value "editMode" and applies the new value to
+        // the DOM
         $scope.$watch('editMode', function(newValue, oldValue) {
-            $(function(){
-                if (newValue) {
-                    $(".table-handle").css("cursor", "all-scroll");
-                } else {
-                    $(".table").css("cursor", "pointer");
-                }
-            });
+            toggleTableStyle(newValue);
         });
 
+        // Options for the tables
         $scope.options = {
             floating: false,
             columns: 14,
             minRows: 5,
 
+            pushing: false,
+            swapping: true,
+
             resizable: {
-                enabled: false
+                enabled: false,
+                handles: ["s", "e", "ne", "se", "sw"]
             },
 
             draggable: {
-                enabled: false,
                 handle: ".table-handle"
             }
         };
 
+        // Default tables
         $scope.tables = [
             { 
-                name: "Blane",
-                points: 3
-            },
-            { 
-                name: "Test",
-                points: 3
+                name: "John Doe",
+                points: 0
             }
         ];
 
+        // What executes when you toggle the Edit Mode
         $scope.toggleEditMode = function(){
             $scope.options.resizable.enabled = $scope.editMode;
-            $scope.options.draggable.enabled = $scope.editMode;
         };
 
+        // What executes when you add a table
         $scope.addTable = function(){
             $scope.tables.push({
                 name: "",
                 points: 0
             });
+
+            $(function(){
+                toggleTableStyle($scope.editMode);
+            });
+
         };
 
         $scope.increment = function(table) {
             table.points += 1;
+
+            changeShade(table.points)
         };
+
+        $scope.decrement = function(table) {
+            table.points -= 1;
+        };
+
+
     }]);
 
     app.directive('table', function(){
@@ -64,6 +99,19 @@
 
         };
     });
+
+    app.directive('ngRightClick', function($parse) {
+        return function(scope, element, attrs) {
+            var fn = $parse(attrs.ngRightClick);
+            element.bind('contextmenu', function(event) {
+                scope.$apply(function() {
+                    event.preventDefault();
+                    fn(scope, {$event:event});
+                });
+            });
+        };
+    });
+
 })();
 
 
